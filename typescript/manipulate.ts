9d42bf447429abf0691d4482bb0535cbde7fcb02
rachata.p <rachata.ptnn@gmail.com>
2024-03-10 15:39:27 +0700

    remove old original files

--- typescript/manipulate.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 
- * @param text string of value example "aabb", "abcde"
- * @returns {string[]} string array of result
- */ 
-export function manipulate(text: string) : string[] {
-    // TODO : Start your code here
-    throw new Error("Not implemented");
-}
--- typescript/smily.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 
- * @param texts list of string [":)", ":(", ":>"]
- * @returns amount of smily face detected
- */
-export function countSmilyFace(texts: string[]) : number {
-    // TODO : Start your code here
-    throw new Error("Not implemented");
-}
